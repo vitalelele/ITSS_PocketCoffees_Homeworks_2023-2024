71ed4e73417eedb55eaf32ee9bd9c9e570940c4b
vitalelele <vitale.antonio02@gmail.com>
2023-12-05 16:43:31 +0100

    Modified variables name and added comments

--- src/main/java/homework1/FindCostOfPermutations.java
+++ src/main/java/homework1/FindCostOfPermutations.java
@@ -7,62 +7,62 @@ import java.util.List;
 import main.java.customException.*;
 
 /**
- * This class calculates all permutations of an array of integers and calculates
- * the cost of each permutation sequence based on a certain rule.
+ This program receives as input an array of elements, the size of the array it takes as input can be a maximum of ten elements.
+ The program calculates all possible permutations, furthermore the user provides as input an integer representing a cost.
+ The program returns all the permutations whose cost is equal to what the user provided as input.
  */
 public class FindCostOfPermutations {
     /**
      * This inner class associates a permutation with its cost.
      */
     private static class CostSum {
-        int[] sequence;
-        int cost;
+        int[] sequenceOfNumbers;
+        int costOfArray;
 
-        public CostSum(int[] sequence, int cost) {
-            this.sequence = sequence;
-            this.cost = cost;
+        public CostSum(int[] sequenceOfNumbers, int costOfArray) {
+            this.sequenceOfNumbers = sequenceOfNumbers;
+            this.costOfArray = costOfArray;
         }
     }
 
     /**
      * This method generates all possible permutations of the array
      * and calculates the cost of all permutations, where the cost of
-     * a permutation is defined as the cumulative sum of the
-     * sequence of possible numbers.
+     * a permutation is given by the sum of all the sums cumulative of the current permutation.
      *
-     * @param numbers array of integers to be permuted
-     * @param costMax the cost to look for in various permutations
+     * @param inputNumbers array of integers to be permuted
+     * @param costRequested the cost to look for in various permutations
      * @return ArrayList of CostSum where the permutations have the requested cost, if there are no permutations
      * whose cost is equal to the requested one, the ArrayList is empty
      * @throws NullNumberException is thrown if the numbers array is null
      * @throws IllegalNumberException is thrown if the array size is 0
      * @throws IllegalCostException is thrown if the cost to look for is less than 0
      */
-    public static ArrayList<CostSum> costOfSums(int[] numbers, int costMax) throws NullNumberException, IllegalNumberException, IllegalCostException {
-        if (numbers != null) {
-            if (numbers.length != 0) {
-                if (costMax > 0) {
-                    int size = numbers.length;
-                    List<int[]> numberPermutations = generatePermutations(numbers);
+    private static ArrayList<CostSum> costOfSums(int[] inputNumbers, int costRequested) throws NullNumberException, IllegalNumberException, IllegalCostException {
+
+        if (inputNumbers != null) {
+            if (inputNumbers.length != 0 && inputNumbers.length <= 10) {
+                if (costRequested > 0) {
+                    int size = inputNumbers.length;
+                    List<int[]> numberPermutations = generatePermutations(inputNumbers);
                     ArrayList<CostSum> sumsOfPermutations = new ArrayList<>();
 
                     int sum;
                     int cost;
-                    for (int i = 0; i < numberPermutations.size(); i++) {
-                        sum = (numberPermutations.get(i))[0] + (numberPermutations.get(i))[1];
+                    for (int[] numberPermutation : numberPermutations) {
+                        sum = numberPermutation[0] + numberPermutation[1];
                         cost = sum;
                         for (int j = 2; j < size; j++) {
-                            sum += numberPermutations.get(i)[j];
+                            sum += numberPermutation[j];
                             cost += sum;
                         }
-                        sumsOfPermutations.add(new CostSum(numberPermutations.get(i).clone(), cost));
+                        sumsOfPermutations.add(new CostSum(numberPermutation.clone(), cost));
                     }
 
                     ArrayList<CostSum> correctPermutations = new ArrayList<>();
-                    int sizePerm = size * size;
-                    for (int i = 0; i < sumsOfPermutations.size(); i++) {
-                        if ((sumsOfPermutations.get(i).cost) == costMax) {
-                            correctPermutations.add(sumsOfPermutations.get(i));
+                    for (CostSum sumsOfPermutation : sumsOfPermutations) {
+                        if ((sumsOfPermutation.costOfArray) == costRequested) {
+                            correctPermutations.add(sumsOfPermutation);
                         }
                     }
 
@@ -71,7 +71,7 @@ public class FindCostOfPermutations {
                     throw new IllegalCostException("The cost to look for in the permutations must be greater than 0");
                 }
             } else {
-                throw new IllegalNumberException("The size of the array must be greater than 0");
+                throw new IllegalNumberException("The size of the array must be greater than 0 and minor than 10.");
             }
         } else {
             throw new NullNumberException("The array cannot be null ");
@@ -79,34 +79,34 @@ public class FindCostOfPermutations {
     }
 
     /**
-     * Generates all possible permutations of the array of integers.
+     * Generates all possible permutations of the array of integers given in input.
      *
-     * @param array array of integers to be permuted
+     * @param arrayToPermutate array of integers to be permuted
      * @return List of permuted arrays
      */
-    public static List<int[]> generatePermutations(int[] array) {
+    public static List<int[]> generatePermutations(int[] arrayToPermutate) {
         List<int[]> result = new ArrayList<>();
-        permute(array, 0, result);
+        permute(arrayToPermutate, 0, result);
         return result;
     }
 
     /**
      * Recursive method to generate permutations for the integer array.
      *
-     * @param array  array of integers for which to generate permutations
+     * @param arrayForPermutation array of integers for which to generate permutations
      * @param start  current index of the permutation
      * @param result list of integer arrays that will contain all generated permutations
      */
-    private static void permute(int[] array, int start, List<int[]> result) {
-        if (start == array.length - 1) {
-            result.add(Arrays.copyOf(array, array.length));
+    private static void permute(int[] arrayForPermutation, int start, List<int[]> result) {
+        if (start == arrayForPermutation.length - 1) {
+            result.add(Arrays.copyOf(arrayForPermutation, arrayForPermutation.length));
             return;
         }
 
-        for (int i = start; i < array.length; i++) {
-            swap(array, start, i);
-            permute(array, start + 1, result);
-            swap(array, start, i); // backtrack
+        for (int i = start; i < arrayForPermutation.length; i++) {
+            swap(arrayForPermutation, start, i);
+            permute(arrayForPermutation, start + 1, result);
+            swap(arrayForPermutation, start, i); // backtrack
         }
     }
 
@@ -114,24 +114,29 @@ public class FindCostOfPermutations {
      * Method that performs the swap between two values in an array.
      *
      * @param array array of integers in which to swap elements
-     * @param i     index of the first element to swap
-     * @param j     index of the second element to swap
+     * @param firstToSwap index of the first element to swap
+     * @param secondToSwap index of the second element to swap
      */
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+    private static void swap(int[] array, int firstToSwap, int secondToSwap) {
+        int temp = array[firstToSwap];
+        array[firstToSwap] = array[secondToSwap];
+        array[secondToSwap] = temp;
     }
 
+    /*  main for testing */
     public static void main(String[] args) throws NullNumberException, IllegalNumberException, IllegalCostException {
-        int[] numbers = new int[]{2, 1, 3};
-        List<CostSum> result = costOfSums(numbers, 11);
-        for (int i = 0; i < result.size(); i++) {
-            System.out.println("Cost: " + result.get(i).cost + " Array: ");
-            for (int j = 0; j < result.get(i).sequence.length; j++) {
-                System.out.println(result.get(i).sequence[j]);
-            }
 
+        int[] inputNumbers = new int[]{2, 1, 3};
+        int costRequested = 9;
+        List<CostSum> result = costOfSums(inputNumbers, costRequested);
+
+        System.out.println("Cost: " + result.get(0).costOfArray);
+        for (CostSum costSum : result) {
+            System.out.print("Array: [");
+            for (int j = 0; j < costSum.sequenceOfNumbers.length; j++) {
+                System.out.print(costSum.sequenceOfNumbers[j] + " ");
+            }
+            System.out.println("]");
         }
     }
 }
